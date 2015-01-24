b560b596c7f133c5558533d916243c8b29809526
psirenny <djtorres0@gmail.com>
2015-01-23 20:35:04 -0500

    push 404 handling to the client, allow req.status() to pass error code, add error callback, prevent redirect loop. Closes issues #1 and #2.

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-var express = require('express');
-
-module.exports = function () {
-  var app = express();
-
-  app.all('*', function (req, res, next) {
-    next('404: ' + req.url);
-  });
-
-  app.use(function (err, req, res, next) {
+module.exports = function (callback) {
+  return function (err, req, res, next) {
     if (!err) return next();
     var status = parseInt(err.message || err.toString());
     status = (status >= 400 && status < 600) ? status : 500;
-    res.redirect('/error/' + status);
-  });
-
-  return app;
+    status = (res.statusCode === 200) ? status : res.statusCode;
+    res.status(status);
+    var url = '/error/' + status;
+    if (callback) callback(req, err);
+    if (req.url === url) return next();
+    res.redirect(url);
+  };
 };
